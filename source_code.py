e7da459f3c7fd695eb58babe5ce14f1bdb832f9f
rvaccone <rvaccone@skiff.com>
2023-02-08 14:16:12 -0500

    Update
    Co-authored-by: HyperTaes <HyperTaes@users.noreply.github.com>
    Co-authored-by: Phillip Anerine <paner225@gmail.com>

--- source_code.py
+++ source_code.py
@@ -1,8 +1,11 @@
 # Phill Anerine, Justus Neumeister, Rocco Vaccone, TaeSeo Um
 # I pledge my honor that I have abided by the Stevens Honor System.
 
-import PrettyTable
+# Imports
+from prettytable import PrettyTable
+import pandas as pd
 
+# Open the test_gedcom.ged file
 gedcom_file = open('./test_gedcom.ged', 'r')
 
 # Create a list of all the lines in the file and removing the '\n' character
@@ -11,6 +14,19 @@ gedcom_lines = [line[:-1] for line in gedcom_file.readlines()]
 # Creating a list of all valid tags
 valid_tags = ['NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'MARR', 'HUSB', 'WIFE', 'CHIL', 'DIV', 'DATE', 'HEAD', 'TRLR', 'NOTE']
 exception_valid_tags = ['INDI', 'FAM']
+
+# Creating a PrettyTable for individuals
+individual_table = PrettyTable()
+individual_table.field_names = ['ID', 'Name', 'Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse']
+
+# Creating a PrettyTable for families
+family_table = PrettyTable()
+family_table.field_names = ['ID', 'Married', 'Divorced', 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name', 'Children']
+
+# Creating a DataFrame for individuals
+individual_dataframe = pd.DataFrame(columns = ['ID', 'Name', 'Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse'])
+
+
 
 # Open the output file
 with open('./output.txt', 'w') as output_file:
@@ -40,3 +56,7 @@ with open('./output.txt', 'w') as output_file:
 
         # Writing the finalized line to the output file
         output_file.write('<-- ' + finalized_line + '\n')
+
+# Printing out the individual and family tables
+print(individual_table)
+print(family_table)
