c19d0caefcbc6fffec3496a8ff288e81bcc39eb3
rvaccone <rvaccone@skiff.com>
2023-02-08 20:47:56 -0500

    Co-authored-by: Justusneumeister <Justusneumeister@users.noreply.github.com> Co-authored-by: HyperTaes <HyperTaes@users.noreply.github.com> Co-authored-by: Phillip Anerine <paner225@gmail.com>

--- source_code.py
+++ source_code.py
@@ -1,62 +1,135 @@
-# Phill Anerine, Justus Neumeister, Rocco Vaccone, TaeSeo Um
+# Phill Anerine, Justus Neumeister, TaeSeo Um, Rocco Vaccone
 # I pledge my honor that I have abided by the Stevens Honor System.
 
 # Imports
 from prettytable import PrettyTable
-import pandas as pd
+from datetime import datetime
 
 # Open the test_gedcom.ged file
-gedcom_file = open('./test_gedcom.ged', 'r')
+gedcom_file = open('./test_gedcom1.ged', 'r')
 
 # Create a list of all the lines in the file and removing the '\n' character
 gedcom_lines = [line[:-1] for line in gedcom_file.readlines()]
 
 # Creating a list of all valid tags
-valid_tags = ['NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'MARR', 'HUSB', 'WIFE', 'CHIL', 'DIV', 'DATE', 'HEAD', 'TRLR', 'NOTE']
+valid_tags = ['NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'MARR',
+              'HUSB', 'WIFE', 'CHIL', 'DIV', 'DATE', 'HEAD', 'TRLR', 'NOTE']
 exception_valid_tags = ['INDI', 'FAM']
 
 # Creating a PrettyTable for individuals
 individual_table = PrettyTable()
-individual_table.field_names = ['ID', 'Name', 'Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse']
+individual_table.field_names = [
+    'ID', 'Name', 'Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse']
 
 # Creating a PrettyTable for families
 family_table = PrettyTable()
-family_table.field_names = ['ID', 'Married', 'Divorced', 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name', 'Children']
+family_table.field_names = ['ID', 'Married', 'Divorced',
+                            'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name', 'Children']
 
-# Creating a DataFrame for individuals
-individual_dataframe = pd.DataFrame(columns = ['ID', 'Name', 'Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse'])
+individuals = []
+curAssoc = ""
+hasStarted = False
+curIndi = []
+
+for line in gedcom_lines:
+    # Splitting the line into a list of elements
+    elements = line.split(" ")
+
+    if (elements[0] == "0" and hasStarted == False and len(elements) > 2 and elements[2] == "INDI"):
+        hasStarted = True
+
+    if (hasStarted and elements[0] == "0" and len(elements) > 2 and elements[2] == "INDI"):
+        hasStarted = True
+        if (len(curIndi) > 0):
+            individuals.append(curIndi)
+            curIndi = []
+        attribute = ["ID", elements[1].strip()]
+        curIndi.append(attribute)
+    elif (hasStarted and len(elements) > 1):
+        tag = elements[1].strip()
+        if (tag == "SEX" or tag == "FAMC" or tag == "HUSB" or tag == "WIFE"):
+            if (tag == "SEX"):
+                attribute = ["Gender", elements[2].strip()]
+                curIndi.append(attribute)
+            elif (tag == "FAMC"):
+                attribute = ["Child", elements[2].strip()]
+                curIndi.append(attribute)
+            elif (tag == "HUSB"):
+                attribute = ["Husband", elements[2].strip()]
+                curIndi.append(attribute)
+            elif (tag == "WIFE"):
+                attribute = ["Wife", elements[2].strip()]
+                curIndi.append(attribute)
+        elif (tag == "NAME"):
+            if (len(elements) > 3):
+                attribute = ["Name", elements[2] + " /" + elements[3].strip()]
+                curIndi.append(attribute)
+            else:
+                attribute = ["Name", elements[2].strip()]
+                curIndi.append(attribute)
+        elif (tag == "BIRT" or tag == "DEAT"):
+            if (tag == "BIRT"):
+                curAssoc = "Birthday"
+            elif (tag == "DEAT"):
+                curAssoc = "Death"
+        elif (tag == "DATE" and curAssoc != ""):
+            attribute = [curAssoc, elements[2] +
+                         " " + elements[3] + " " + elements[4]]
+            curIndi.append(attribute)
+            curAssoc = ""
+
+    elif (hasStarted):
+        if (tag == "BIRT" or tag == "DEAT"):
+            if (tag == "BIRT"):
+                curAssoc = "Birthday"
+            elif (tag == "DEAT"):
+                curAssoc = "Death"
+        elif (tag == "DATE" and curAssoc != ""):
+            attribute = [curAssoc, elements[2] +
+                         " " + elements[3] + " " + elements[4]]
+            curIndi.append(attribute)
+            curAssoc = ""
 
 
+# print(individuals)
 
-# Open the output file
-with open('./output.txt', 'w') as output_file:
-    # Iterate through the list of lines
-    for line in gedcom_lines:
-        # Writing the line to the output file
-        output_file.write('--> ' + line + '\n')
-        
-        # Split the line into a list of words
-        split_line = line.split(' ')
+def calculate_age(birthday):
+    today = date.today()
+    return today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
 
-        # Check if there is a valid tag
-        if len(split_line) > 1 and split_line[1] in valid_tags:
-            split_line.insert(2, 'Y')
-        
-        # Check if there is an exception valid tag
-        elif len(split_line) > 2 and split_line[2] in exception_valid_tags:
-            split_line.insert(1, split_line.pop(2))
-            split_line.insert(2, 'Y')
-        
-        # If there is no valid tag
+
+# Iterating through the individuals list and adding them to the PrettyTable
+for individual in individuals:
+    # Creating a dictionary of all the values for the individual
+    individual_values = {
+        'ID': 'N/A',
+        'Name': 'N/A',
+        'Gender': 'N/A',
+        'Birthday': 'N/A',
+        'Age': 'N/A',
+        'Alive': 'N/A',
+        'Death': 'N/A',
+        'Child': 'N/A',
+        'Spouse': 'N/A'
+    }
+
+    # Iterating through the attributes of the individual
+    for attributes in individual:
+        individual_values[attributes[0]] = attributes[1]
+
+    individual_values['Alive'] = True if individual_values['Death'] == 'N/A' else False
+
+    if individual_values['Birthday'] != 'N/A':
+        if individual_values['Death'] == 'N/A':
+            death_year = datetime.today().year
         else:
-            split_line.insert(2, 'N')
+            death_year = int(individual_values['Death'].split()[-1])
+        birth_year = int(individual_values['Birthday'].split()[-1])
+        age = death_year - birth_year
+        individual_values['Age'] = age
 
-        # Formatting the line
-        finalized_line = '|'.join(split_line[:3]) + '|' + ' '.join(split_line[3:])
+    # Adding the individual to the PrettyTable
+    individual_table.add_row(individual_values.values())
 
-        # Writing the finalized line to the output file
-        output_file.write('<-- ' + finalized_line + '\n')
-
-# Printing out the individual and family tables
+# Printing the PrettyTable
 print(individual_table)
-print(family_table)
