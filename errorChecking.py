09c7fc4b00880798304b4cb0e019f943864db8a0
HyperTaes <20taeseo@gmail.com>
2023-02-15 16:15:27 -0500

    pushing commit

--- errorChecking.py
+++ errorChecking.py
@@ -32,7 +32,7 @@ def individuals_error_checking(individual_list):
         if individual['Age'] != 'N/A' and individual['Spouse'] != 'N/A':
             assert(int(individual['Age']) >= 14, 'Individual is too young to be married')
 
-        # Check that each birth date is before the current Date
+        # [US01] - Justus
         if individual['Birthday'] != 'N/A':
             date = individual['Birthday'].split()
             day, month, year = int(date[0]), int(
@@ -40,7 +40,7 @@ def individuals_error_checking(individual_list):
             curDay, curMonth, curYear = datetime.today().day, datetime.today().month, datetime.today().year
             assert(curYear > year or (year == curYear and curMonth > month) or (year == curYear and curMonth == month and day < curDay))
 
-        # Check that each death date is before the current Date
+        # [US01] - Justus
         if individual['Death'] != 'N/A':
             date = individual['Death'].split()
             day, month, year = int(date[0]), int(
@@ -52,7 +52,7 @@ def individuals_error_checking(individual_list):
 # Error checking the families
 def families_error_checking(family_list, individual_list):
     for family in family_list:
-        # if marriage date is after death date of either spouse, assert false
+        # [US06] - TaeSeo
         if family['Marriage Date'] != 'N/A' and family['Divorce Date'] and family['Husband ID'] != 'N/A' and family['Wife ID'] != 'N/A':
             for individual in individual_list:
                 if individual['ID'] == family['Husband ID']:
@@ -64,5 +64,8 @@ def families_error_checking(family_list, individual_list):
                     divorceDate = datetime.strptime(family['Divorce Date'], '%d %b %Y')
                     wifeDeathDate = datetime.strptime(individual['Death'], '%d %b %Y')
                     assert(divorceDate < wifeDeathDate)
+
+        # [US05] - Justus
+        
     
     
