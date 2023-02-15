bbdbd8ae011bd8e5b9d7b6256ae3cd8cb763f478
HyperTaes <20taeseo@gmail.com>
2023-02-15 17:51:28 -0500

    unitTest for age 150

--- individualUnitTests.py
+++ individualUnitTests.py
@@ -1,5 +1,6 @@
 # Imported packages
 import unittest
+import errorChecking as ec
 
 # Imported files
 from projectDictionaries import *
@@ -11,10 +12,60 @@ class IndividualTests(unittest.TestCase):
 
     # Creating a function to add an individual to the list
     def create_individual(self, individual_dict):
-        self.individual_list.append(individual_dict.copy())
+        individual = individual_dict.copy()
+        self.individual_list.append(individual)
+        return individual
 
     # Creating a function to return the list of individuals
     def get_individual_list(self):
         return self.individual_list
 
-# ProjectIndividualTests = IndividualTests()
+
+
+def checkIndividualNotTooOld():
+    individual = IndividualTests()
+    individual.create_individual(individual_dict)['Age'] = 200
+    try:
+        ec.individuals_error_checking(individual.get_individual_list())
+        print('Individual is not too old:' + str(individual.get_individual_list()[0]['Age']))
+    except:
+        print('Failed successfully with error:' + str(individual.get_individual_list()[0]['Age']) )
+
+    individual = IndividualTests()
+    individual.create_individual(individual_dict)['Age'] = 100
+    try:
+        ec.individuals_error_checking(individual.get_individual_list())
+        print('Individual is not too old:' + str(individual.get_individual_list()[0]['Age']))
+    except:
+        print('Failed successfully with error:' + str(individual.get_individual_list()[0]['Age']) )
+
+    individual = IndividualTests()
+    individual.create_individual(individual_dict)['Age'] = 70
+    try:
+        ec.individuals_error_checking(individual.get_individual_list())
+        print('Individual is not too old:' + str(individual.get_individual_list()[0]['Age']))
+    except:
+        print('Failed successfully with error:' + str(individual.get_individual_list()[0]['Age']) )
+
+    individual = IndividualTests()
+    individual.create_individual(individual_dict)['Age'] = 300
+    try:
+        ec.individuals_error_checking(individual.get_individual_list())
+        print('Individual is not too old:' + str(individual.get_individual_list()[0]['Age']))
+    except:
+        print('Failed successfully with error:' + str(individual.get_individual_list()[0]['Age']) )
+
+
+    individual = IndividualTests()
+    individual.create_individual(individual_dict)['Age'] = 10
+    try:
+        ec.individuals_error_checking(individual.get_individual_list())
+        print('Individual is not too old:' + str(individual.get_individual_list()[0]['Age']))
+    except:
+        print('Failed successfully with error:' + str(individual.get_individual_list()[0]['Age']) )
+    
+
+
+checkIndividualNotTooOld()
+
+
