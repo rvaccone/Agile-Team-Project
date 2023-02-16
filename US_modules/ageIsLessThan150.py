5c93c08afb609e58b53c7bfb693ca4e9a85d0957
HyperTaes <20taeseo@gmail.com>
2023-02-16 17:49:30 -0500

    why is individual test gone

--- US_modules/ageIsLessThan150.py
+++
@@ -1,5 +0,0 @@
-# [US07] - TaeSeo
-def ageIsLessThan150(individual_list):
-    for individual in individual_list:
-        if individual['Age'] != 'N/A':
-            assert(int(individual['Age']) < 150), "Individual is too old"
