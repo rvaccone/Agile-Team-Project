9df5b3fab54a3ca5db82f68f2ac9b04bd1631476
justus <jneumeist@gmail.com>
2023-02-16 14:47:57 -0500

    new user stories

--- US_modules/DateBeforeCurrent.py
+++ US_modules/DateBeforeCurrent.py
@@ -1,5 +1,5 @@
 from datetime import datetime, date
-def dateBeforeCurrentDate (individual_list):
+def dateBeforeCurrentDate (individual_list,family_list):
     for individual in individual_list:
         if individual['Birthday'] != 'N/A':
             date = individual['Birthday'].split()
@@ -7,3 +7,12 @@ def dateBeforeCurrentDate (individual_list):
                 datetime.strptime(date[1], "%b").month), int(date[2])
             curDay, curMonth, curYear = datetime.today().day, datetime.today().month, datetime.today().year
             assert(curYear > year or (year == curYear and curMonth > month) or (year == curYear and curMonth == month and day < curDay)), "Birth date is after today's date"
+    for family in family_list:
+        if family['Marriage Date'] != 'N/A':
+            date = datetime.strptime(family['Marriage Date'], '%d %b %Y').date()
+            curDate = datetime.now()
+            assert(date < curDate), "Marriage date is after today's date"
+        if family['Divorce Date'] != 'N/A':
+            date = datetime.strptime(family['Divorce Date'], '%d %b %Y').date()
+            curDate = datetime.now()
+            assert(date < curDate), "Divorce date is after today's date"
---
+++ US_modules/MarriageBeforeDeath.py
@@ -0,0 +1,17 @@
+from datetime import datetime, date
+def MarriageBeforeDeath (individual_list,family_list):
+    for family in family_list:
+        if family['Marriage Date'] != 'N/A':
+                for individual in individual_list:
+                    if individual['ID'] == family['Husband ID']:
+                        if individual['Death'] != 'N/A':
+                            marriageDate = datetime.strptime(family['Marriage Date'], '%d %b %Y').date()
+                            husbandDeathDate = datetime.strptime(individual['Death'], '%d %b %Y').date()
+                            assert(marriageDate < husbandDeathDate), "Marriage date is after husband's death date"
+                    
+
+                    if individual['ID'] == family['Wife ID']:
+                        if individual['Death'] != 'N/A':
+                            marriageDate = datetime.strptime(family['Marriage Date'], '%d %b %Y').date()
+                            wifeDeathDate = datetime.strptime(individual['Death'], '%d %b %Y').date()
+                            assert(marriageDate < wifeDeathDate), "Marriage date is after wife's death date"
