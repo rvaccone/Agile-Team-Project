ac50470432c0ba0dd62a21ecc2942ab7e9b3d651
HyperTaes <20taeseo@gmail.com>
2023-02-16 14:36:04 -0500

    added userstori

---
+++ US_modules/ageIsLessThan150.py
@@ -0,0 +1,4 @@
+def ageIsLessThan150(individual_list):
+    for individual in individual_list:
+        if individual['Age'] != 'N/A':
+            assert(int(individual['Age']) < 150), "Individual is too old"
