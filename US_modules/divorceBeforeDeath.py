27c2b480ad7d38c7bb6ee5997b2f0a9cf57d8801
HyperTaes <20taeseo@gmail.com>
2023-02-22 10:38:02 -0500

    updated divorceBeforeDeath

--- US_modules/divorceBeforeDeath.py
+++ US_modules/divorceBeforeDeath.py
@@ -1,19 +1,20 @@
 #[US06] - TaeSeo
 import datetime 
 
+import datetime 
+
+def is_divorce_before_death(person_id, death_date, divorce_date):
+    if death_date != 'N/A':
+        death_date = datetime.strptime(death_date, '%d %b %Y').date()
+        divorce_date = datetime.strptime(divorce_date, '%d %b %Y').date()
+        assert(divorce_date < death_date), f"Divorce date is after {person_id}'s death date"
+
 def divorceBeforeDeath(individual_list, family_list):
     for family in family_list:
         if family['Divorce Date'] != 'N/A':
             for individual in individual_list:
                 if individual['ID'] == family['Husband ID']:
-                    if individual['Death'] != 'N/A':
-                        divorceDate = datetime.strptime(family['Divorce Date'], '%d %b %Y').date()
-                        husbandDeathDate = datetime.strptime(individual['Death'], '%d %b %Y').date()
-                        assert(divorceDate < husbandDeathDate), "Divorce date is after husband's death date"
-                
+                    is_divorce_before_death('husband', individual['Death'], family['Divorce Date'])
 
                 if individual['ID'] == family['Wife ID']:
-                    if individual['Death'] != 'N/A':
-                        divorceDate = datetime.strptime(family['Divorce Date'], '%d %b %Y').date()
-                        wifeDeathDate = datetime.strptime(individual['Death'], '%d %b %Y').date()
-                        assert(divorceDate < wifeDeathDate), "Divorce date is after wife's death date"
+                    is_divorce_before_death('wife', individual['Death'], family['Divorce Date'])
